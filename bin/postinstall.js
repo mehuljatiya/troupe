7bc66fb4f1d2c5af5446e8b2a0b05e385c4032d0
Mehul Jatiya <mehuljatiya@gmail.com>
2026-05-27 15:18:04 +0530

    Add postinstall message to guide users to run troupe setup

---
+++ bin/postinstall.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+console.log('');
+console.log('✅ cashfree-troupe installed!');
+console.log('');
+console.log('👉 Get started by running:');
+console.log('');
+console.log('   troupe setup');
+console.log('');
